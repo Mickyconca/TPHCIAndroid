d12f77701c2e3c763007c8043f89e2dbffe7ce92
Patrick <patrickmdey@hotmail.com>
2020-11-11 11:43:54 -0300

    Nav(falta conectarlos)

--- QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/LogInActivity.java
+++ QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/LogInActivity.java
@@ -2,15 +2,13 @@ package ar.edu.itba.quickfitness;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SingleActivityApp extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
--- QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/RoutinesFragment.java
+++ QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/RoutinesFragment.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.quickfitness;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,21 +13,20 @@ import java.util.Collections;
 
 public class RoutinesFragment extends Fragment {
 
-    private RoutineAdapter adapter;
-    private RoutinesFragment binding;
+    //private RoutinesFragment binding;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ArrayList<String> routineNames = new ArrayList<>();
         String[] names = {"Rutina 1", "Rutina 2", "Rutina 3", "Rutina 4"};
         Collections.addAll(routineNames, names);
 
-        adapter = new RoutineAdapter(routineNames);
+        RoutineAdapter adapter = new RoutineAdapter(routineNames);
         //binding.routineCardList;    //lleno la lista usando el adapter que creé
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.routine__card_list, container, false);
+        return inflater.inflate(R.layout.routine_card_list, container, false);
     }
 }
