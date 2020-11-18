39c069d12312019450c5cc31bd50e2efbc5c3764
Patrick <patrickmdey@hotmail.com>
2020-11-18 13:28:45 -0300

    Contraseña

--- QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/LogInActivity.java
+++ QuickFitness/app/src/main/java/ar/edu/itba/quickfitness/LogInActivity.java
@@ -2,11 +2,15 @@ package ar.edu.itba.quickfitness;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ar.edu.itba.quickfitness.api.ApiClient;
 import ar.edu.itba.quickfitness.api.ApiUserService;
@@ -20,10 +24,23 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.loginButton).setOnClickListener(v -> {
+        getSupportActionBar().hide();
 
-            EditText username = (EditText) findViewById(R.id.putUsername);
-            EditText password = (EditText) findViewById(R.id.putPassword);
+        EditText username = findViewById(R.id.putUsername);
+        EditText password = findViewById(R.id.putPassword);
+        ImageButton showPassword = findViewById(R.id.showPasswordButton);
+        int covered = password.getInputType();
+
+        AtomicInteger aux = new AtomicInteger();
+        showPassword.setOnClickListener(v->{
+            if(aux.get() %2 == 0)
+                password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            else
+                password.setInputType(covered);
+            aux.getAndIncrement();
+        });
+
+        findViewById(R.id.loginButton).setOnClickListener(v -> {
 
             if (username.getText().toString().length() <= 0)
                 Toast.makeText(this, R.string.invalid_username, Toast.LENGTH_LONG).show();
